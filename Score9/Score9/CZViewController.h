75a0c60ba3ae86750cff13994dc56abb8bcf7d15
James Sodini <jsodini@mac.com>
2013-02-15 00:51:47 -0800

    Ignore Xcode user data and .DS_Store files

---
+++ Score9/Score9/CZViewController.h
@@ -0,0 +1,13 @@
+//
+//  CZViewController.h
+//  Score9
+//
+//  Created by James Sodini on 2/15/13.
+//  Copyright (c) 2013 Cocoa Zombie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CZViewController : UIViewController
+
+@end
---
+++ Score9/Score9/CZViewController.m
@@ -0,0 +1,29 @@
+//
+//  CZViewController.m
+//  Score9
+//
+//  Created by James Sodini on 2/15/13.
+//  Copyright (c) 2013 Cocoa Zombie. All rights reserved.
+//
+
+#import "CZViewController.h"
+
+@interface CZViewController ()
+
+@end
+
+@implementation CZViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
