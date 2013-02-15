e9f216353aff22d759dd3a25b8490a17592ea585
James Sodini <jsodini@mac.com>
2013-02-15 01:34:46 -0800

    Connect IBOutlets to storyboard

--- Score9/CZViewController.h
+++ Score9/CZViewController.h
@@ -10,4 +10,34 @@
 
 @interface CZViewController : UIViewController
 
+//
+// Player One
+//
+
+@property (strong, nonatomic) IBOutlet UILabel *playerOneNextPointLabel;
+@property (strong, nonatomic) IBOutlet UILabel *playerOneScoreLabel;
+@property (strong, nonatomic) IBOutlet UILabel *playerOneWinLabel;
+@property (strong, nonatomic) IBOutlet UILabel *playerOneSkillLabel;
+@property (strong, nonatomic) IBOutlet UIButton *playerOnePointAddButton;
+@property (strong, nonatomic) IBOutlet UIButton *playerOnePointSubtractButton;
+@property (strong, nonatomic) IBOutlet UIButton *playerOneSkillAddButton;
+@property (strong, nonatomic) IBOutlet UIButton *playerOneSkillSubtractButton;
+@property (strong, nonatomic) IBOutlet UIProgressView *playerOneNextPointBar;
+@property (strong, nonatomic) IBOutlet UIProgressView *playerOneWinBar;
+
+//
+// Player Two
+//
+
+@property (strong, nonatomic) IBOutlet UILabel *playerTwoNextPointLabel;
+@property (strong, nonatomic) IBOutlet UILabel *playerTwoScoreLabel;
+@property (strong, nonatomic) IBOutlet UILabel *playerTwoWinLabel;
+@property (strong, nonatomic) IBOutlet UILabel *playerTwoSkillLabel;
+@property (strong, nonatomic) IBOutlet UIButton *playerTwoPointAddButton;
+@property (strong, nonatomic) IBOutlet UIButton *playerTwoPointSubtractButton;
+@property (strong, nonatomic) IBOutlet UIButton *playerTwoSkillAddButton;
+@property (strong, nonatomic) IBOutlet UIButton *playerTwoSkillSubtractButton;
+@property (strong, nonatomic) IBOutlet UIProgressView *playerTwoNextPointBar;
+@property (strong, nonatomic) IBOutlet UIProgressView *playerTwoWinBar;
+
 @end
